7d625cb65bc94a2b9279609402f43cc4ed4351e7
shrouk <shroukmahrous99@gmail.com>
2024-12-04 16:54:31 +0200

    + initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const NewsApp());
+}
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
